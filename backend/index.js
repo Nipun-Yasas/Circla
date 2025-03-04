4d872345ab1301264e06c16fb134d7db4a477482
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-03-04 17:59:07 +0530

    completed Authentication and Authorization

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
 import { register } from './controllers/auth.js';
 
 //Configerations
@@ -18,18 +19,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
-app.use(morgan('common'));
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
-app.use("/assets",express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets",express.static(path.join(__dirname, "public/assets")));
 
 //File Storage
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/assets');
+        cb(null, "public/assets");
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
@@ -39,10 +40,12 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 //Routes with files
-app.post('/auth/register',upload.single("picture"), register);
+app.post("/auth/register",upload.single("picture"), register);
+
+//Routes
+app.use("/auth",authRoutes);
 
 //MONGOOSE SETUP
-
 const PORT = process.env.PORT || 6001;
 
 mongoose.connect(process.env.MONGO_URL, {
